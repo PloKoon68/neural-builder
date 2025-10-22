c8005282852e4091d287c9077bfb4b9bd36d7303
PloKoon68 <ors_mehmet68@hotmail.com>
2025-10-22 01:07:00 -0700

    frontend backend access fixed

--- view/src/api/axious.js
+++ view/src/api/axious.js
@@ -1,15 +1,16 @@
 // For ES6 modules / TypeScript
 import axios from 'axios';
 
-const EXPRESS_URL = "http://localhost:5000/api"; // Change this to your backend URL
-//const EXPRESS_URL = "https://deep-learning-framework-express.onrender.com/api"; // Change this to your backend URLs
+//const EXPRESS_URL = "http://localhost:5000/api"; // Change this to your backend URL
+const EXPRESS_URL = "https://deep-learning-framework-express.onrender.com/api"; // Change this to your backend URLs
 //
+/*
 const CROW_URL_HTTP = "http://localhost:18080"; // Change this to your backend URL
 const CROW_URL_WS = "ws://localhost:18080/ws"; // Change this to your backend URL
-/*
+*/
 const CROW_URL_HTTP = "https://deep-learning-framework-crow.onrender.com"; 
 const CROW_URL_WS = "wss://deep-learning-framework-crow.onrender.com/ws"; 
-*/
+
 // Create axios instance pointing to your C++ backend
 const crowAPI = axios.create({
     // Replace with your C++ backend server address and port
